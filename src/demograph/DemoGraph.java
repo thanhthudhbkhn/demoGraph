2a4d6b1fe2faeb2d53ac4832ca121aa3b2c02bfb
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-03-18 22:42:04 +0700

    create graph successful with large file

--- src/demograph/DemoGraph.java
+++ src/demograph/DemoGraph.java
@@ -16,19 +16,19 @@ public class DemoGraph {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
-        //khoi tao mang chua 10 list, moi list 500 ptu
+        //khoi tao mang chua 10 list
         //moi list la danh sach dinh ke cua dinh co verteId = listId
-        List<Integer> list[] = new  LinkedList[500];
-        for(int i=0; i<=10; i++) {
+        List<Integer> list[] = new  LinkedList[5000];
+        for(int i=0; i<5000; i++) {
             list[i] = new LinkedList<>();
         }        
-        
         //khai bao path de thao tac voi file
         String path = "/home/thanhthu/NetBeansProjects/demoGraph/src";
-        BufferedReader in = new BufferedReader(new FileReader(path+"/demograph/data.txt"));
-//        BufferedReader in = new BufferedReader(new FileReader(path+"/facebook/facebook_combined.txt"));
+//        BufferedReader in = new BufferedReader(new FileReader(path+"/demograph/data.txt"));
+        BufferedReader in = new BufferedReader(new FileReader(path+"/facebook/facebook_combined.txt"));
         
         int m = 0; //so edge cua graph
+        int n = 0; //verteId max cua graph
         String currentLine = in.readLine(); //currentLine doc tu file, co dang "v1 v2"
         while(currentLine != null){
             m++;            
@@ -37,15 +37,17 @@ public class DemoGraph {
                 String[] vertes = currentLine.split(" ");
                 int v1 = Integer.parseInt(vertes[0]);
                 int v2 = Integer.parseInt(vertes[1]);
+                //get max verteId
+                n = (v1>v2)? v1:v2;
                 //add 2 vertes into lists
                 list[v1].add(v2);
                 list[v2].add(v1);
             }
             currentLine = in.readLine();
         }
-        
+        in.close();
         //display the lists to check
-        for (int i = 0; i <=10; i++) {
+        for (int i = 0; i <=n; i++) {
             System.out.println(i+" - "+list[i]);
         }
     }
