e6591cb30a512b0b83ca5f5828c03462919473c4
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-03-18 16:09:39 +0700

    create graph with linked list successfull

--- src/demograph/DemoGraph.java
+++ src/demograph/DemoGraph.java
@@ -9,6 +9,7 @@ import java.util.*;
  *
  * @author ThuPT
  */
+
 public class DemoGraph {
 
     /**
@@ -17,15 +18,31 @@ public class DemoGraph {
     public static void main(String[] args) {
         // TODO code application logic here
         List<Integer> list[] = new  LinkedList[500];
-        for(int i=0; i<5; i++) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of vertices: ");
+        int n = scanner.nextInt();
+        for(int i=0; i<=n; i++) {
             list[i] = new LinkedList<>();
+        }        
+        System.out.println("Enter number of edges: ");
+        int m = scanner.nextInt();
+        
+        scanner.nextLine();
+        for(int i=0;i<m;i++) {
+            System.out.println("Enter the edge: ");
+            String edge = scanner.nextLine();
+            if (!"".equals(edge)) {
+                String[] vertes = edge.split(" ");
+            
+                int v1 = Integer.parseInt(vertes[0]);
+                int v2 = Integer.parseInt(vertes[1]);
+                list[v1].add(v2);
+                list[v2].add(v1);
+            }
         }
-        list[0].add(10);
-        list[1].add(10); list[1].add(20);
-        list[2].add(10);
-        list[3].add(10);
-        list[4].add(10);
-        for (int i = 0; i < 5; i++) {
+        
+        for (int i = 0; i <=n; i++) {
             System.out.println(i+" - "+list[i]);
         }
     }
