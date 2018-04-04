60c8003fbe42266613e50d7e8d5b23155eba21d4
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-04-04 22:06:49 +0700

    fix typo

--- src/demograph/DemoGraph.java
+++ src/demograph/DemoGraph.java
@@ -15,9 +15,9 @@ import java.io.*;
 public class DemoGraph {
     static List<Integer> list[] = new  LinkedList[5000];
     int m = 0; //so edge cua graph
-    int n = 0; //verteId max cua graph
+    int n = 0; //vertexId max cua graph
     
-    public void BFS(int verteId) {
+    public void BFS(int vertexId) {
         // Mark all the vertices as not visited(By default
         // set as false)
         boolean visited[] = new boolean[5000];
@@ -26,21 +26,21 @@ public class DemoGraph {
         LinkedList<Integer> queue = new LinkedList<>();
  
         // Mark the current node as visited and enqueue it
-        visited[verteId]=true;
-        queue.add(verteId);
+        visited[vertexId]=true;
+        queue.add(vertexId);
  
         while (!queue.isEmpty())
         {
             // Dequeue a vertex from queue and print it
-            verteId = queue.poll();
-            System.out.print(verteId+" ");
+            vertexId = queue.poll();
+            System.out.print(vertexId+" ");
 
             // Get all adjacent vertices of the dequeued vertex 
             // If a adjacent has not been visited, then mark it
             // visited and enqueue it            
-            int itemCount = list[verteId].size();
+            int itemCount = list[vertexId].size();
             for (int i = 0; i < itemCount; i++) {
-                int adjacentVerte = list[verteId].get(i);
+                int adjacentVerte = list[vertexxId].get(i);
                 if (visited[adjacentVerte]==false) {
                     visited[adjacentVerte]=true;
                     queue.add(adjacentVerte);
@@ -49,27 +49,27 @@ public class DemoGraph {
         }
     }
     
-    public void DFSFunction(int verteId, boolean visited[]) {
+    public void DFSFunction(int vertexId, boolean visited[]) {
         // Mark the current node as visited and enqueue it
-        visited[verteId]=true; 
-        System.out.print(verteId+" ");
+        visited[vertexId]=true; 
+        System.out.print(vertexId+" ");
 
         // Get all adjacent vertices of the vertex 
         // If a adjacent has not been visited, visit it with DFS
-        int itemCount = list[verteId].size();
+        int itemCount = list[vertexId].size();
         for (int i = 0; i < itemCount; i++) {
-            int adjacentVerte = list[verteId].get(i);
+            int adjacentVerte = list[vertexId].get(i);
             if (visited[adjacentVerte]==false) {
                 DFSFunction(adjacentVerte,visited);
             }
         }
     }
     
-    public void DFS(int verteId) {
+    public void DFS(int vertexId) {
         // Mark all the vertices as not visited(By default
         // set as false)
         boolean visited[] = new boolean[5000];
-        DFSFunction(verteId, visited);        
+        DFSFunction(vertexId, visited);        
     }
     
     
@@ -81,7 +81,7 @@ public class DemoGraph {
     
     public List<Integer>[] createGraph() throws FileNotFoundException, IOException {
         //khoi tao mang chua 10 list
-        //moi list la danh sach dinh ke cua dinh co verteId = listId
+        //moi list la danh sach dinh ke cua dinh co vertexId = listId
         
         for(int i=0; i<5000; i++) {
             list[i] = new LinkedList<>();
@@ -95,14 +95,14 @@ public class DemoGraph {
         while(currentLine != null){
             m++;            
             if (!"".equals(currentLine)) {
-                //get 2 vertes from an edge
-                String[] vertes = currentLine.split(" ");
-                int v1 = Integer.parseInt(vertes[0]);
-                int v2 = Integer.parseInt(vertes[1]);
-                //get max verteId
+                //get 2 vertexs from an edge
+                String[] vertices = currentLine.split(" ");
+                int v1 = Integer.parseInt(vertices[0]);
+                int v2 = Integer.parseInt(vertices[1]);
+                //get max vertexId
                 n = (n>v1)? n:v1;
                 n = (n>v2)? n:v2;
-                //add 2 vertes into lists
+                //add 2 vertices into lists
                 list[v1].add(v2);
                 list[v2].add(v1);
             }
