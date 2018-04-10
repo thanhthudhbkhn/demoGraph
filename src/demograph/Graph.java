28c7a19bf93719f48665812879b3ce1537033a0c
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-04-10 16:33:18 +0700

    fix add edge

--- src/demograph/Graph.java
+++ src/demograph/Graph.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package demograph;
+
 import java.util.*;
 import java.io.*;
 
@@ -11,12 +12,12 @@ import java.io.*;
  *
  * @author ThuPT
  */
-
 public class Graph {
+
     ArrayList<Vertex> ds_dinh = new ArrayList<>();
     int m = 0; //so edge cua graph
     int n = 0; //vertexId max cua graph
-    
+
 //    public void BFS(int vertexId) {
 //        // Mark all the vertices as not visited(By default
 //        // set as false)
@@ -48,7 +49,6 @@ public class Graph {
 //            }
 //        }
 //    }
-    
 //    public void DFSFunction(int vertexId, boolean visited[]) {
 //        // Mark the current node as visited and enqueue it
 //        visited[vertexId]=true; 
@@ -71,27 +71,26 @@ public class Graph {
 //        boolean visited[] = new boolean[5000];
 //        DFSFunction(vertexId, visited);        
 //    }
-
     public Graph() {
-        
+
     }
-        
-    public void displayGraph(){
+
+    public void displayGraph() {
         for (int i = 0; i < ds_dinh.size(); i++) {
             Vertex v = ds_dinh.get(i);
             System.out.println(v.vertexId + " " + v.adjacencyList);
         }
     }
-    
-    public int[] getVerticesFromString(String str){
+
+    public int[] getVerticesFromString(String str) {
         //get 2 vertexs from an line "v1 v2"
-        int[] result = new int[2];        
+        int[] result = new int[2];
         String[] vertices = str.split(" ");
         result[0] = Integer.parseInt(vertices[0]);
         result[1] = Integer.parseInt(vertices[1]);
         return result;
     }
-    
+
     public boolean CointainVertex(int vertexId) {
         for (int i = 0; i < ds_dinh.size(); i++) {
             if (ds_dinh.get(i).getVertexId() == vertexId) {
@@ -100,19 +99,19 @@ public class Graph {
         }
         return false;
     }
-    
-    public void addVertex(Vertex vertex){
+
+    public void addVertex(Vertex vertex) {
         ds_dinh.add(vertex);
     }
-    
+
     public void addEdge(String type, int v1, int v2) {
-        if ("directed".equals(type)){
+        if ("directed".equals(type)) {
             if (CointainVertex(v1) == false) {
                 Vertex v = new Vertex(v1);
                 v.adjacencyList.add(v2);
                 addVertex(v);
             } else {
-                Vertex v = ds_dinh.get(v1);
+                Vertex v = ds_dinh.get(indexOf(v1));
                 v.adjacencyList.add(v2);
             }
         } else if ("undirected".equals(type)) {
@@ -120,8 +119,8 @@ public class Graph {
             addEdge("directed", v2, v1);
         }
     }
-    
-    public int indexOf(int vertexId){
+
+    public int indexOf(int vertexId) {
         for (int i = 0; i < ds_dinh.size(); i++) {
             if (vertexId == ds_dinh.get(i).getVertexId()) {
                 return i;
@@ -129,28 +128,28 @@ public class Graph {
         }
         return -1;
     }
-    
+
     public ArrayList createGraph() throws FileNotFoundException, IOException {
         //khai bao path de thao tac voi file
         String path = "/home/thanhthu/NetBeansProjects/demoGraph/src";
 //        BufferedReader in = new BufferedReader(new FileReader(path+"/facebook/facebook_combined.txt"));
-        try (BufferedReader in = new BufferedReader(new FileReader(path+"/demograph/data.txt"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(path + "/demograph/data.txt"))) {
             //try (BufferedReader in = new BufferedReader(new FileReader(path+"/facebook/facebook_combined.txt"))) {
             String currentLine = in.readLine(); //currentLine doc tu file, co dang "v1 v2"
-            while(currentLine != null){
+            while (currentLine != null) {
                 m++;
                 if (!"".equals(currentLine)) {
                     int vertex1 = getVerticesFromString(currentLine)[0];
                     int vertex2 = getVerticesFromString(currentLine)[1];
                     //add 2 vertices into lists
-                    addEdge("undirected",vertex1, vertex2);
+                    addEdge("undirected", vertex1, vertex2);
                 }
                 currentLine = in.readLine();
             }
         } //currentLine doc tu file, co dang "v1 v2"
         return ds_dinh;
     }
-    
+
 //    public void displayPath(int path[],int vertex1, int vertex2) {
 ////        int destination = vertex2;
 //        System.out.print(vertex2);
