4788787a71f0f9af986c64922db399881670826b
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-04-11 19:25:03 +0700

    fix bug

--- src/demograph/Graph.java
+++ src/demograph/Graph.java
@@ -18,11 +18,11 @@ public class Graph {
 
     //return true if BFS is successful
     public boolean BFS(int vertexId) {
-        // Mark all the vertices as not visited(By default
-        // set as false)
         if (containVertex(vertexId) == false) {
             return false;
         }
+        // Mark all the vertices as not visited(By default
+        // set as false)
         ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(ds_dinh.size(), false));
         // Create a queue for BFS
         LinkedList<Integer> queue = new LinkedList<>();
@@ -160,59 +160,64 @@ public class Graph {
         return ds_dinh;
     }
 
-//    public void displayPath(int path[],int vertex1, int vertex2) {
-////        int destination = vertex2;
-//        System.out.print(vertex2);
-//        do {
-//            int step = path[vertex2];
-//            vertex2 = step;
-//            System.out.print("<-"+vertex2);
-//        } while (vertex2!=vertex1);
-//    }
-//    
-//    public boolean findPath (int vertex1, int vertex2) {
-//        //if 2 vertices are the same, return true
-//        if (vertex1 == vertex2 && vertex1<=n) return true;
-//        int start = vertex1;
-//        // Mark all the vertices as not visited(By default
-//        // set as false)
-//        boolean visited[] = new boolean[5000];
-//        int path[] = new int[5000];
-//        // Create a queue for BFS
-//        LinkedList<Integer> queue = new LinkedList<>();
-// 
-//        // Mark the current node as visited and enqueue it
-//        visited[vertex1]=true;
-//        queue.add(vertex1);
-////        path[0]=vertex1;
-// 
-//        while (!queue.isEmpty())
-//        {
-//            // Dequeue a vertex from queue and print it
-//            vertex1 = queue.poll();
-//            //System.out.print(vertex1+" ");
-//
-//            // Get all adjacent vertices of the dequeued vertex 
-//            // If a adjacent has not been visited, then mark it
-//            // visited and enqueue it            
-//            int itemCount = list[vertex1].size();
-//            for (int i = 0; i < itemCount; i++) {
-//                int adjacentVertex = list[vertex1].get(i);
-//                if (visited[adjacentVertex]==false) {
-//                    visited[adjacentVertex]=true;
-//                    //If this vertex is the destination return true
-//                    if (adjacentVertex == vertex2) {
-//                        path[adjacentVertex] = vertex1;
-//                        displayPath(path,start,vertex2);
-//                        return true;
-//                    }
-//                    //Else continue BFS
-//                    queue.add(adjacentVertex);
-//                    path[adjacentVertex] = vertex1;
-////                    System.out.print(adjacentVertex+"->");
-//                }
-//            }
-//        }
-//        return false;
-//    }
+    public void displayPath(int path[], int vertex1, int vertex2) {
+//        int destination = vertex2;
+        System.out.print(vertex2);
+        do {
+            int step = path[vertex2];
+            vertex2 = step;
+            System.out.print(" <- " + vertex2);
+        } while (vertex2 != vertex1);
+    }
+
+    public boolean findPath(int vertex1, int vertex2) {
+        //if vertex1 or vertex2 are not existed in graph, return false
+        if (containVertex(vertex1) == false || containVertex(vertex2) == false) {
+            return false;
+        }
+        // else if 2 vertices are the same, return true
+        if (vertex1 == vertex2) {
+            return true;
+        }
+        //else
+        int start = vertex1;
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(ds_dinh.size(), false));
+        int[] path = new int[5000];
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        // Mark the current node as visited and enqueue it
+        visited.set(indexOf(vertex1), true);
+        queue.add(vertex1);
+//        path[0]=vertex1;
+
+        while (!queue.isEmpty()) {
+            // Dequeue a vertex from queue and print it
+            vertex1 = queue.poll();
+            //System.out.print(vertex1+" ");
+
+            // Get all adjacent vertices of the dequeued vertex 
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it            
+            Vertex vertex = getVertex(vertex1);
+            for (int i = 0; i < vertex.adjacencyList.size(); i++) {
+                int adjacentVertex = vertex.adjacencyList.get(i);
+                if (visited.get(indexOf(adjacentVertex)) == false) {
+                    visited.set(indexOf(adjacentVertex), true);
+                    //If this vertex is the destination return true
+                    if (adjacentVertex == vertex2) {
+                        path[adjacentVertex] = vertex1;
+                        displayPath(path, start, vertex2);
+                        return true;
+                    }
+                    //Else continue BFS
+                    queue.add(adjacentVertex);
+                    path[adjacentVertex] = vertex1;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/demograph/Main.java
+++ src/demograph/Main.java
@@ -13,24 +13,32 @@ import java.util.Scanner;
  * @author thanhthu
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Graph g = new Graph();
         g.createGraph();
         g.displayGraph();
-//        System.out.println("Enter 2 vertices: ");
+        System.out.println("Enter 2 vertices: ");
         Scanner scn = new Scanner(System.in);
-//        int v1 = scn.nextInt();
-//        int v2 = scn.nextInt();
-//        g.findPath(v1,v2);
-        
+        int v1 = scn.nextInt();
+        int v2 = scn.nextInt();
+        if (g.findPath(v1, v2) == false) {
+            System.out.println("There is not any paths from " + v1 + " to " + v2 + ".");
+        }
+        System.out.println("");
+
         System.out.print("Breadth First Traversal from: ");
         int v = scn.nextInt();
-        if(g.BFS(v)==false) System.out.println("Breadth First Traversal failed.");
+        if (g.BFS(v) == false) {
+            System.out.println("Breadth First Traversal failed.");
+        }
         System.out.println("");
-        
+
         System.out.print("Depth First Traversal from: ");
         v = scn.nextInt();
-        if(g.DFS(v)==false) System.out.println("Breadth First Traversal failed.");
+        if (g.DFS(v) == false) {
+            System.out.println("Breadth First Traversal failed.");
+        }
         System.out.println("");
     }
 }
