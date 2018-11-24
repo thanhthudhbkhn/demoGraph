4439e1446611a47723d3ab267fcff3a0603c3d0c
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-11-25 00:29:06 +0700

    bfs & dfs

--- src/demograph/Graph.java
+++ src/demograph/Graph.java
@@ -14,223 +14,224 @@ import java.io.*;
  */
 public class Graph {
 
-    ArrayList<Vertex> ds_dinh = new ArrayList<>();
-    int so_canh = 0;
+	Hashtable<Integer, Vertex> ds_dinh = new Hashtable<Integer, Vertex>();
+	int so_canh = 0;
 
-    //return true if BFS is successful
-    public boolean BFS(int vertexId) {
-        if (containVertex(vertexId) == false) {
-            return false;
-        }
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(ds_dinh.size(), false));
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<>();
+//	return true if BFS is successful
+	public boolean BFS(int vertexId) {
+		if (ds_dinh.containsKey(vertexId) == false) {
+			return false;
+		}
+		// Mark all the vertices as not visited(By default
+		// set as false)
+		ArrayList<Integer> visited = new ArrayList<Integer>();
+		// Create a queue for BFS
+		LinkedList<Integer> queue = new LinkedList<>();
 
-        // Mark the current node as visited and enqueue it
-        visited.set(indexOf(vertexId), true);
-        queue.add(vertexId);
+		// Mark the current node as visited and enqueue it
+		visited.add(vertexId);
+		queue.add(vertexId);
 
-        while (!queue.isEmpty()) {
-            // Dequeue a vertex from queue and print it
-            vertexId = queue.poll();
-//            System.out.print(vertexId + " ");
+		while (!queue.isEmpty()) {
+			// Dequeue a vertex from queue and print it
+			vertexId = queue.poll();
+//			System.out.print(vertexId + " ");
 
-            // Get all adjacent vertices of the dequeued vertex 
-            Vertex vertex = getVertex(vertexId);
-            for (int i = 0; i < vertex.adjacencyList.size(); i++) {
-                int adjacentVertex = vertex.adjacencyList.get(i);
-                // If a adjacent has not been visited, then mark it
-                // visited and enqueue it    
-                if (visited.get(indexOf(adjacentVertex)) == false) {
-                    visited.set(indexOf(adjacentVertex), true);
-                    queue.add(adjacentVertex);
-                }
-            }
-        }
-        return true;
-    }
+			// Get all adjacent vertices of the dequeued vertex 
+			Vertex vertex = ds_dinh.get(vertexId);
+			for (int i = 0; i < vertex.adjacencyList.size(); i++) {
+				int adjacentVertex = vertex.adjacencyList.get(i);
+				// If a adjacent has not been visited, then mark it
+				// visited and enqueue it    
+				if (visited.contains(adjacentVertex) == false) {
+					visited.add(adjacentVertex);
+					queue.add(adjacentVertex);
+				}
+			}
+		}
+		return true;
+	}
 
-    public void DFSFunction(int vertexId, ArrayList<Boolean> visited) {
-        // Mark the current node as visited and enqueue it
-        visited.set(indexOf(vertexId), true);
+	public void DFSFunction(int vertexId, ArrayList<Integer> visited) {
+		// Mark the current node as visited and enqueue it
+		visited.add(vertexId);
 //        System.out.print(vertexId + " ");
 
-        // Get all adjacent vertices of the vertex 
-        Vertex vertex = getVertex(vertexId);
-        for (int i = 0; i < vertex.adjacencyList.size(); i++) {
-            int adjacentVertex = vertex.adjacencyList.get(i);
-            // If a adjacent has not been visited, visit it with DFS
-            if (visited.get(indexOf(adjacentVertex)) == false) {
-                DFSFunction(adjacentVertex, visited);
-            }
-        }
-    }
+		// Get all adjacent vertices of the vertex 
+		Vertex vertex = ds_dinh.get(vertexId);
+		for (int i = 0; i < vertex.adjacencyList.size(); i++) {
+			int adjacentVertex = vertex.adjacencyList.get(i);
+			// If a adjacent has not been visited, visit it with DFS
+			if (visited.contains(adjacentVertex) == false) {
+				DFSFunction(adjacentVertex, visited);
+			}
+		}
+	}
 
-    public boolean DFS(int vertexId) {
-        if (containVertex(vertexId) == false) {
-            return false;
-        }
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(ds_dinh.size(), false));
-        DFSFunction(vertexId, visited);
-        return true;
-    }
+	public boolean DFS(int vertexId) {
+		if (ds_dinh.containsKey(vertexId) == false) {
+			return false;
+		}
+		// Mark all the vertices as not visited(By default
+		// set as false)
+		ArrayList<Integer> visited = new ArrayList<>();
+		DFSFunction(vertexId, visited);
+		return true;
+	}
 
-    public void displayGraph() {
-        for (int i = 0; i < ds_dinh.size(); i++) {
-            Vertex v = ds_dinh.get(i);
-            System.out.println(v.vertexId + " " + v.adjacencyList);
-        }
-    }
-    
-    public void displayGraphInfo() {
-        System.out.println("There are:");
-        System.out.println(ds_dinh.size()+" vertices");
-        System.out.println("and "+so_canh+" edges");
-        System.out.println("in this graph.");
-    }
+	public void displayGraph() {
+		Enumeration data = ds_dinh.keys();
+		while (data.hasMoreElements()) {
+			Vertex v = ds_dinh.get(data.nextElement());
+			System.out.println(v.vertexId + " " + v.adjacencyList);
 
-    public int[] getVerticesFromString(String str) {
-        //get 2 vertexs from an line "v1 v2"
-        int[] result = new int[2];
-        //String[] vertices = {v1,v2}
-        String[] vertices = str.split(" ");
-        //Convert from string into integer
-        result[0] = Integer.parseInt(vertices[0]);
-        result[1] = Integer.parseInt(vertices[1]);
-        return result;
-    }
+		}
+	}
 
-    public Vertex getVertex(int vertexId) {
-        Vertex v = ds_dinh.get(indexOf(vertexId));
-        return v;
-    }
+	public void displayGraphInfo() {
+		System.out.println("There are:");
+		System.out.println(ds_dinh.size() + " vertices");
+		System.out.println("and " + so_canh + " edges");
+		System.out.println("in this graph.");
+	}
 
-    //return true if the list contain the vertex
-    public boolean containVertex(int vertexId) {
-        for (int i = 0; i < ds_dinh.size(); i++) {
-            if (ds_dinh.get(i).getVertexId() == vertexId) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public int[] getVerticesFromString(String str) {
+		//get 2 vertexs from an line "v1 v2"
+		int[] result = new int[2];
+		//String[] vertices = {v1,v2}
+		String[] vertices = str.split(" ");
+		//Convert from string into integer
+		result[0] = Integer.parseInt(vertices[0]);
+		result[1] = Integer.parseInt(vertices[1]);
+		return result;
+	}
 
-    public void addVertex(Vertex vertex) {
-        ds_dinh.add(vertex);
-    }
+	public void addVertex(Vertex vertex) {
+		ds_dinh.put(vertex.vertexId, vertex);
+	}
 
-    public void addEdge(String type, int v1, int v2) {
-        if ("directed".equals(type)) {
-            //if there are not vertex v1, create new vertex before add edge
-            if (containVertex(v1) == false) {
-                Vertex v = new Vertex(v1);
-                v.adjacencyList.add(v2);
-                addVertex(v);
-            } else {
-                //if vertex v1 is existed, get v1 and add v2 to v1's adjacency list
-                Vertex v = getVertex(v1);
-                v.adjacencyList.add(v2);
-                if (containVertex(v2) == false) {
-                    v = new Vertex(v2);
-                    addVertex(v);
-                }
-            }
-        } else if ("undirected".equals(type)) {
-            addEdge("directed", v1, v2);
-            addEdge("directed", v2, v1);
-        }
-    }
+	public void addEdge(String type, int v1, int v2) {
+		if ("directed".equals(type)) {
+			//if there are not vertex v1, create new vertex before add edge
+			if (ds_dinh.containsKey(v1) == false) {
+				Vertex v = new Vertex(v1);
+				v.adjacencyList.add(v2);
+				addVertex(v);
+			} else {
+				//if vertex v1 is existed, get v1 and add v2 to v1's adjacency list
+				Vertex v = ds_dinh.get(v1);
+				v.adjacencyList.add(v2);
+				if (ds_dinh.containsKey(v2) == false) {
+					v = new Vertex(v2);
+					addVertex(v);
+				}
+			}
+		} else if ("undirected".equals(type)) {
+			addEdge("directed", v1, v2);
+			addEdge("directed", v2, v1);
+		}
+	}
 
-    //return the index of the vertex in the list
-    public int indexOf(int vertexId) {
-        for (int i = 0; i < ds_dinh.size(); i++) {
-            if (vertexId == ds_dinh.get(i).getVertexId()) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	//return the index of the vertex in the list
+	public int indexOf(int vertexId) {
+		for (int i = 0; i < ds_dinh.size(); i++) {
+			if (vertexId == ds_dinh.get(i).getVertexId()) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-    public ArrayList createGraph() throws FileNotFoundException, IOException {
-        //define path for open file with shorter address
-        String path = "D:/gradute/demoGraph/src";
-        try (BufferedReader in = new BufferedReader(new FileReader(path + "/facebook/facebook_combined.txt"))) {
-            String currentLine = in.readLine(); //currentLine has format: "v1 v2"
-            while (currentLine != null) {
-                if (!"".equals(currentLine)) {
-                    int vertex1 = getVerticesFromString(currentLine)[0];
-                    int vertex2 = getVerticesFromString(currentLine)[1];
-                    //add 2 vertices into lists
-                    addEdge("undirected", vertex1, vertex2);
-                    so_canh++;
-                }
-                currentLine = in.readLine();
-            }
-        } //currentLine doc tu file, co dang "v1 v2"
-        return ds_dinh;
-    }
+	public Hashtable createGraph() throws FileNotFoundException, IOException {
+		//define path for open file with shorter address
+		String path = "D:/gradute/demoGraph/src";
+		try (BufferedReader in = new BufferedReader(new FileReader(path + "/facebook/facebook_combined.txt"))) {
+			String currentLine = in.readLine(); //currentLine has format: "v1 v2"
+			while (currentLine != null) {
+				if (!"".equals(currentLine)) {
+					int vertex1 = getVerticesFromString(currentLine)[0];
+					int vertex2 = getVerticesFromString(currentLine)[1];
+					//add 2 vertices into lists
+					addEdge("undirected", vertex1, vertex2);
+					so_canh++;
+				}
+				currentLine = in.readLine();
+			}
+		} //currentLine doc tu file, co dang "v1 v2"
+//		addEdge("undirected", 1, 2);
+//		so_canh++;
+//		addEdge("undirected", 1, 3);
+//		so_canh++;
+//		addEdge("undirected", 1, 4);
+//		so_canh++;
+//		addEdge("undirected", 1, 5);
+//		so_canh++;
+//		addEdge("undirected", 4, 2);
+//		so_canh++;
+//		addEdge("undirected", 5, 2);
+//		so_canh++;
+		displayGraph();
 
-    public void displayPath(int path[], int vertex1, int vertex2) {
+		return ds_dinh;
+	}
+
+	public void displayPath(int path[], int vertex1, int vertex2) {
 //        int destination = vertex2;
-        System.out.print(vertex2);
-        do {
-            int step = path[vertex2];
-            vertex2 = step;
-            System.out.print(" <- " + vertex2);
-        } while (vertex2 != vertex1);
-    }
+		System.out.print(vertex2);
+		do {
+			int step = path[vertex2];
+			vertex2 = step;
+			System.out.print(" <- " + vertex2);
+		} while (vertex2 != vertex1);
+	}
 
-    public boolean findPath(int vertex1, int vertex2) {
-        //if vertex1 or vertex2 are not existed in graph, return false
-        if (containVertex(vertex1) == false || containVertex(vertex2) == false) {
-            System.out.println("The vertex does not exist.");
-            return false;
-        }
-        // else if 2 vertices are the same, return true
-        if (vertex1 == vertex2) {
-            return true;
-        }
-        //else
-        int start = vertex1;
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(ds_dinh.size(), false));
-        int[] path = new int[10000]; //do lon cua mang phai > chi so lon nhat cua dinh
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<>();
+	public boolean findPath(int vertex1, int vertex2) {
+		//if vertex1 or vertex2 are not existed in graph, return false
+		if (ds_dinh.containsKey(vertex1) == false || ds_dinh.containsKey(vertex2) == false) {
+			System.out.println("The vertex does not exist.");
+			return false;
+		}
+		// else if 2 vertices are the same, return true
+		if (vertex1 == vertex2) {
+			return true;
+		}
+		//else
+		int start = vertex1;
+		// Mark all the vertices as not visited(By default
+		// set as false)
+		ArrayList<Integer> visited = new ArrayList<>();
+		int[] path = new int[10000]; //do lon cua mang phai > chi so lon nhat cua dinh
+		// Create a queue for BFS
+		LinkedList<Integer> queue = new LinkedList<>();
 
-        // Mark the current node as visited and enqueue it
-        visited.set(indexOf(vertex1), true);
-        queue.add(vertex1);
+		// Mark the current node as visited and enqueue it
+		visited.add(vertex1);
+		queue.add(vertex1);
 
-        while (!queue.isEmpty()) {
-            // Dequeue a vertex from queue and print it
-            vertex1 = queue.poll();
+		while (!queue.isEmpty()) {
+			// Dequeue a vertex from queue and print it
+			vertex1 = queue.poll();
 
-            // Get all adjacent vertices of the dequeued vertex 
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it            
-            Vertex vertex = getVertex(vertex1);
-            for (int i = 0; i < vertex.adjacencyList.size(); i++) {
-                int adjacentVertex = vertex.adjacencyList.get(i);
-                if (visited.get(indexOf(adjacentVertex)) == false) {
-                    visited.set(indexOf(adjacentVertex), true);
-                    //If this vertex is the destination return true
-                    if (adjacentVertex == vertex2) {
-                        path[adjacentVertex] = vertex1;
-                        displayPath(path, start, vertex2);
-                        return true;
-                    }
-                    //Else continue BFS
-                    queue.add(adjacentVertex);
-                    path[adjacentVertex] = vertex1;
-                }
-            }
-        }
-        return false;
-    }
+			// Get all adjacent vertices of the dequeued vertex 
+			// If a adjacent has not been visited, then mark it
+			// visited and enqueue it            
+			Vertex vertex = ds_dinh.get(vertex1);
+			for (int i = 0; i < vertex.adjacencyList.size(); i++) {
+				int adjacentVertex = vertex.adjacencyList.get(i);
+				if (visited.contains(adjacentVertex) == false) {
+					visited.add(adjacentVertex);
+					//If this vertex is the destination return true
+					if (adjacentVertex == vertex2) {
+						path[adjacentVertex] = vertex1;
+						displayPath(path, start, vertex2);
+						return true;
+					}
+					//Else continue BFS
+					queue.add(adjacentVertex);
+					path[adjacentVertex] = vertex1;
+				}
+			}
+		}
+		return false;
+	}
 }
--- src/demograph/Main.java
+++ src/demograph/Main.java
@@ -6,6 +6,7 @@
 package demograph;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 /**
@@ -14,67 +15,68 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Graph g = new Graph();
-        g.createGraph();
-        
-        int choice = 0;
-        do {
-            System.out.println("-----------------------------------------");
-            System.out.println("Press 1 to display graph info.");
-            System.out.println("Press 2 find path between 2 vertices.");
-            System.out.println("Press 3 to start Breadth First Traversal.");
-            System.out.println("Press 4 to start Deepth First Traversal.");
-            System.out.println("-----------------------------------------");
-            
-            Scanner scn = new Scanner(System.in);
-            choice = scn.nextInt();
-        
-            switch(choice){
-                case 1: 
-                    g.displayGraphInfo();
-                    break;
-                case 2:
-                    System.out.println("Enter 2 vertices: ");
-                    int v1 = scn.nextInt();
-                    int v2 = scn.nextInt();
-                    long start = System.nanoTime();
-                    if (g.findPath(v1, v2) == false) {
-                        System.out.println("There is not any paths from " + v1 + " to " + v2 + ".");
-                    }
-                    float runTime = (System.nanoTime() - start)/1000000f;
-                    System.out.println("");
-                    System.out.println("It takes "+runTime+"ms to find path.");
-                    break;
-                case 3:
-                    int v=0;
-                    start = System.nanoTime();
-                    for (int i = 0; i < g.ds_dinh.size(); i++) {
-                        v = g.ds_dinh.get(i).vertexId;
-                        if (g.BFS(v) == false) {
-                            System.out.println("Breadth First Traversal failed with vertex ."+v);
-                        }
-//                        System.out.println("");
-                    }
-                    runTime = (System.nanoTime() - start)/1000000f;
-//                    System.out.println("");
-                    System.out.println("It takes "+runTime+"ms to run BFS.");
-                    break;
-                case 4:
-                    start = System.nanoTime();
-                    for (int i = 0; i < g.ds_dinh.size(); i++) {
-                        v = g.ds_dinh.get(i).vertexId;
-                        if (g.DFS(v) == false) {
-                            System.out.println("Deepth First Traversal failed.");
-                        }
-                        System.out.println("");
-                    }
-                    runTime = (System.nanoTime() - start)/1000000f;
-//                    System.out.println("");
-                    System.out.println("It takes "+runTime+"ms to run DFS.");
-                    break;
-                default:break;
-            }
-        } while (choice==1||choice==2||choice==3||choice==4);
-    }
+	public static void main(String[] args) throws IOException {
+		Graph g = new Graph();
+		g.createGraph();
+
+		int choice = 0;
+		do {
+			System.out.println("-----------------------------------------");
+			System.out.println("Press 1 to display graph info.");
+			System.out.println("Press 2 find path between 2 vertices.");
+			System.out.println("Press 3 to start Breadth First Traversal.");
+			System.out.println("Press 4 to start Deepth First Traversal.");
+			System.out.println("-----------------------------------------");
+
+			Scanner scn = new Scanner(System.in);
+			choice = scn.nextInt();
+
+			switch (choice) {
+				case 1:
+					g.displayGraphInfo();
+					break;
+				case 2:
+					System.out.println("Enter 2 vertices: ");
+					int v1 = scn.nextInt();
+					int v2 = scn.nextInt();
+					long start = System.nanoTime();
+					if (g.findPath(v1, v2) == false) {
+						System.out.println("There is not any paths from " + v1 + " to " + v2 + ".");
+					}
+					float runTime = (System.nanoTime() - start) / 1000000f;
+					System.out.println("");
+					System.out.println("It takes " + runTime + "ms to find path.");
+					break;
+				case 3:
+					start = System.nanoTime();
+					Enumeration data = g.ds_dinh.keys();
+					while (data.hasMoreElements()) {
+						Vertex v = g.ds_dinh.get(data.nextElement());
+						if (g.BFS(v.vertexId) == false) {
+							System.out.println("Breadth First Traversal failed with vertex ." + v);
+						}
+					}
+					runTime = (System.nanoTime() - start) / 1000000f;
+					System.out.println("");
+					System.out.println("It takes " + runTime + "ms to run BFS.");
+					break;
+				case 4:
+					start = System.nanoTime();
+					data = g.ds_dinh.keys();
+					while (data.hasMoreElements()) {
+						Vertex v = g.ds_dinh.get(data.nextElement());
+						if (g.DFS(v.vertexId) == false) {
+							System.out.println("Deepth First Traversal failed with vertex ." + v);
+						}
+//						System.out.println("");
+					}
+					runTime = (System.nanoTime() - start) / 1000000f;
+					System.out.println("");
+					System.out.println("It takes " + runTime + "ms to run DFS.");
+					break;
+				default:
+					break;
+			}
+		} while (choice == 1 || choice == 2 || choice == 3 || choice == 4);
+	}
 }
